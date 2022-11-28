9c10f1784ccb651bfff2ff45f1ecb4c0ee4c8579
vishal9934 <117971824+vishal9934@users.noreply.github.com>
2022-11-29 01:05:49 +0530

    Add files via upload

---
+++ Airlinesystem/src/main/java/com/mycompany/airlinesystem/JoptionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.airlinesystem;
+
+/**
+ *
+ * @author vk993
+ */
+class JoptionPane {
+
+    static void showMessegeDialog(searchCustomer aThis, String record_not_present) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ Airlinesystem/src/main/java/com/mycompany/airlinesystem/resultSet.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.airlinesystem;
+
+/**
+ *
+ * @author vk993
+ */
+class resultSet {
+
+    void next() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    Object getString(String maXid) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
